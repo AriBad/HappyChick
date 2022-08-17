ff93f13df4425dff19a7599062c4ad02ec5e8b8c
Filixo <alaeboughaba@gmail.com>
2022-08-17 15:56:17 +0200

    Usr

--- src/main/java/happyChick/dao/IDAOPoulailler.java
+++ src/main/java/happyChick/dao/IDAOPoulailler.java
@@ -5,8 +5,11 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import happyChick.model.Poulailler;
+import happyChick.model.User;
 
 public interface IDAOPoulailler extends JpaRepository<Poulailler, Integer> {
 	@Query("SELECT p from Poulailler p join fetch p.listePoules where p.id=:id")
 	public Poulailler findByIdWithPoule(@Param("id") Integer id );
+
+	public void deleteByUser(User user);
 }
--- src/main/java/happyChick/dao/IDAOPoule.java
+++ src/main/java/happyChick/dao/IDAOPoule.java
@@ -5,11 +5,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import happyChick.model.Poulailler;
 import happyChick.model.Poule;
 import happyChick.model.Temperament;
+import happyChick.model.User;
 
 public interface IDAOPoule extends JpaRepository<Poule, Integer> {
 
 	void deleteByPoulailler(Poulailler poulailler);
 
 	void deleteByTemperament(Temperament temperament);
+
 
 }
