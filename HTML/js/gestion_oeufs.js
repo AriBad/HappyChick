aa327fa36e592f2194b9f1246c3ea98b6f1e658b
AlbanFl <alban.flandin@gmail.com>
2022-09-12 15:48:09 +0200

    Ajout vérif gestion oeufs

--- HTML/js/gestion_oeufs.js
+++ HTML/js/gestion_oeufs.js
@@ -1,13 +1,32 @@
 
 function calculOeufs() {
+    //calcul oeufs
     oeufs = totalOeuf.innerHTML
-    oeufs -= (40 * nourritureAchat.value)
+    oeufs -= (50 * nourritureAchat.value)
     if(securite.checked) {
         oeufs -= 20;
     }
     if (taille.checked) {
         oeufs -= 20;
     }
+
+    //met à jour les input avec les oeufs restants
+    nourritureAchat.max=oeufs%50
+
+    if(oeufs<20 && !securite.checked) {
+        securite.disabled=true;
+    }
+    else {
+        securite.disabled=false;
+    }
+
+    if(oeufs<20 && !taille.checked) {
+        taille.disabled=true;
+    }
+    else {
+        taille.disabled=false;
+    }
+
     oeufsDispos.innerHTML= oeufs;
 }
 
