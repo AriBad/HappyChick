6a1b792b4b0df3ca52602faaa9d38ba28fc9fd09
AriBad <ariane.badoual@gmail.com>
2022-09-14 17:17:36 +0200

    adding filtre for detail poule

--- happychick-angular/src/app/details-poule/details-poule.component.ts
+++ happychick-angular/src/app/details-poule/details-poule.component.ts
@@ -11,15 +11,71 @@ import { PouleHttpService } from '../poule-http.service';
 })
 export class DetailsPouleComponent implements OnInit {
   poules: Array<Poule>;
+  vivantes: boolean = true;
+  mortes: boolean = false;
+  filtre: string = null;
 
   constructor(private pouleService: PouleHttpService, private poulaillerService: PoulaillerHttpService, private poulaillerSessionService: PoulaillerSessionService) {
-  
-   }
-   getSessionPoulailler(): Poulailler {
+
+  }
+
+  getSessionPoulailler(): Poulailler {
     return this.poulaillerSessionService.poulailler;
   }
 
-  enVoyage(id: number){
+  filterPoule(mortes: boolean, vivantes: boolean, filtre: string): Array<Poule> {
+    console.log(filtre);
+    console.log(vivantes);
+    console.log(mortes);
+    if (filtre == "poussins") {
+      console.log("ce sont des poussins");
+      if (mortes && vivantes) {
+        return this.getSessionPoulailler().listePoules.filter(poule => poule.poussin == true);
+      } else if (mortes && !vivantes) {
+        return this.getSessionPoulailler().listePoules.filter(poule => poule.causeMort != null && poule.poussin == true)
+      } else if (!mortes && vivantes) {
+        return this.getSessionPoulailler().listePoules.filter(poule => poule.causeMort == null && poule.poussin == true)
+      } else {
+        return null;
+      }
+    } else if (filtre == "coq") {
+      console.log("ce sont des coqs");
+      if (mortes && vivantes) {
+        return this.getSessionPoulailler().listePoules.filter(poule => poule.femelle == false);
+      } else if (mortes && !vivantes) {
+        return this.getSessionPoulailler().listePoules.filter(poule => poule.causeMort != null && poule.femelle == false)
+      } else if (!mortes && vivantes) {
+        return this.getSessionPoulailler().listePoules.filter(poule => poule.causeMort == null && poule.femelle == false)
+      } else {
+        return null;
+      }
+    } else if (filtre == null || filtre=="") {
+      console.log("ce sont toutes");
+      if (mortes && vivantes) {
+        return this.getSessionPoulailler().listePoules
+      } else if (mortes && !vivantes) {
+        return this.getSessionPoulailler().listePoules.filter(poule => poule.causeMort != null)
+      } else if (!mortes && vivantes) {
+        return this.getSessionPoulailler().listePoules.filter(poule => poule.causeMort == null)
+      } else {
+        return null;
+      }
+    } else {
+      if (mortes && vivantes) {
+        console.log("ce sont des poules");
+        return this.getSessionPoulailler().listePoules.filter(poule => poule.temperament == filtre)
+      } else if (mortes && !vivantes) {
+        return this.getSessionPoulailler().listePoules.filter(poule => poule.temperament == filtre && poule.causeMort != null)
+      } else if (!mortes && vivantes) {
+        return this.getSessionPoulailler().listePoules.filter(poule => poule.temperament == filtre && poule.causeMort == null)
+      } else {
+        return null;
+      }
+    }
+
+  }
+
+  enVoyage(id: number) {
     this.pouleService.delete(id);
   }
 
